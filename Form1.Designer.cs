8427645adfd14e93218ecc8173148a9807f58c02
wivivies <vikaberezhna77@gmail.com>
2023-09-28 22:00:21 +0300

    Lab 1 was done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonYes = new System.Windows.Forms.Button();
+            this.buttonNo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.BackColor = System.Drawing.Color.Green;
+            this.buttonYes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonYes.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.buttonYes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonYes.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonYes.ForeColor = System.Drawing.Color.Black;
+            this.buttonYes.Location = new System.Drawing.Point(124, 273);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(205, 51);
+            this.buttonYes.TabIndex = 0;
+            this.buttonYes.Text = "Yes";
+            this.buttonYes.UseVisualStyleBackColor = false;
+            this.buttonYes.MouseClick += new System.Windows.Forms.MouseEventHandler(this.BtYesClickEvent);
+            // 
+            // buttonNo
+            // 
+            this.buttonNo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttonNo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonNo.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttonNo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNo.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonNo.ForeColor = System.Drawing.Color.Black;
+            this.buttonNo.Location = new System.Drawing.Point(453, 273);
+            this.buttonNo.Name = "buttonNo";
+            this.buttonNo.Size = new System.Drawing.Size(205, 51);
+            this.buttonNo.TabIndex = 1;
+            this.buttonNo.Text = "No";
+            this.buttonNo.UseVisualStyleBackColor = false;
+            this.buttonNo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BtNoMouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Snap ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(147, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(496, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Are you satisfied with your salary?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonNo);
+            this.Controls.Add(this.buttonYes);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Question";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button buttonYes;
+        private Button buttonNo;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,23 @@ namespace meet1winform
         {
             InitializeComponent();
         }
+
+        private void BtYesClickEvent(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Well, we had no doubt that you think so!");
+        }
+
+        private void BtNoMouseMove(object sender, MouseEventArgs e)
+        {
+            buttonNo.Top -= e.Y;
+            buttonNo.Left += e.X;
+
+            if(buttonNo.Top < -10 || buttonNo.Top > 100)
+            {
+                buttonNo.Top = 60;
+            }
+            if (buttonNo.Left < -80 || buttonNo.Left > 250)
+                buttonNo.Left = 120;
+        }
     }
 }
