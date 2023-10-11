1556250ffe87c405707bfcffa27fc8a831d83a62
wivivies <vikaberezhna1@gmail.com>
2023-10-11 23:52:37 +0300

    hw ex2 done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnPlay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlay.Font = new System.Drawing.Font("Showcard Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnPlay.Location = new System.Drawing.Point(189, 125);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(434, 49);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play a new game";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.Font = new System.Drawing.Font("Showcard Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnExit.Location = new System.Drawing.Point(189, 342);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(434, 49);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnInfo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInfo.Font = new System.Drawing.Font("Showcard Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnInfo.Location = new System.Drawing.Point(189, 232);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(434, 49);
+            this.btnInfo.TabIndex = 2;
+            this.btnInfo.Text = "Information";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelQuestion.Location = new System.Drawing.Point(22, 38);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(766, 35);
+            this.labelQuestion.TabIndex = 3;
+            this.labelQuestion.Text = "Do you wanna play with me? Tap Play right now!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.labelQuestion);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPlay);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Number Guess Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnPlay;
+        private Button btnExit;
+        private Button btnInfo;
+        private Label labelQuestion;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,69 @@ namespace meet1winform
 {
     public partial class Form1 : Form
     {
+        private int max = 2000;
+        private int min = 1;
+        private int attempts;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank you for playing our game!", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            Application.Exit();
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            string gameRules = "Welcome to the Number Guessing Game!\n\n" +
+                              "In this game, you think of a number between 1 and 2000, and the computer will try to guess it using a binary search algorithm.\n\n" +
+                              "You'll respond to the computer's guesses with 'Yes' if the guessed number is correct, 'No, higher' if the number is higher, or 'No, smaller' if the number is smaller.\n\n" +
+                              "The game continues until the computer successfully guesses your number or you start a new game.\n\n" +
+                              "Have fun and enjoy the game!";
+
+            MessageBox.Show(gameRules, "Game Rules", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Think of a number between 1 and 2000, and press 'Yes' to start the game.", "Number Guess Game", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                attempts = 0;
+                PlayGame(min, max);
+            }
+            else
+            {
+                // Ничего не делаем, игра не началась
+            }
+        }
+
+        private void PlayGame(int min, int max)
+        {
+            int guess = (min + max) / 2; // Вычисляем среднее значение
+
+            attempts++;
+
+            DialogResult result = MessageBox.Show($"Is the guessed number: {guess}?\n\nYes - Number Guessed\nNo - Number is Higher\nCancel - Number is Smaller", "Number Guess Game", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                MessageBox.Show($"The program guessed your number ({guess}) in {attempts} attempts. Do you want to play again?", "Number Guess Game", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            else if (result == DialogResult.No)
+            {
+                PlayGame(guess + 1, max); // Число больше
+            }
+            else if (result == DialogResult.Cancel)
+            { 
+                PlayGame(min, guess - 1); // Число меньше
+            }
+        }
+
     }
 }
