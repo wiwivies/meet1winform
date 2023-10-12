6ea88b090519a7089fc9a0ef80a4acdc10f0fd50
wivivies <vikaberezhna1@gmail.com>
2023-10-12 20:59:52 +0300

    ex 3 done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lb_cord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_cord
+            // 
+            this.lb_cord.AutoSize = true;
+            this.lb_cord.BackColor = System.Drawing.Color.MediumPurple;
+            this.lb_cord.Font = new System.Drawing.Font("Showcard Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_cord.Location = new System.Drawing.Point(9, 11);
+            this.lb_cord.Name = "lb_cord";
+            this.lb_cord.Size = new System.Drawing.Size(126, 21);
+            this.lb_cord.TabIndex = 0;
+            this.lb_cord.Text = "Mouse Move: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.MediumPurple;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.lb_cord);
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(1000, 600);
+            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ex 3";
+            this.Load += new System.EventHandler(this.MainFormLoad);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KD_Form);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KU_Form);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MMEvent);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label lb_cord;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,88 @@ namespace meet1winform
 {
     public partial class Form1 : Form
     {
+        private bool isCTRLdown = false;
+        private string originalTitle;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void MainFormLoad(object sender, EventArgs e)
+        {
+            originalTitle = this.Text;
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                if(isCTRLdown == true)
+                {
+                    this.Close();
+                }
+
+                var currentPoint = e.Location;
+                Rectangle rect = new Rectangle(10, 10, this.ClientSize.Width - 20, this.ClientSize.Height - 20);
+
+                if (rect.Contains(currentPoint))
+                {
+                    MessageBox.Show("Точка находится внутри прямоугольника.");
+                }
+                else if (
+                    (currentPoint.X == rect.Left || currentPoint.X == rect.Right) ||
+                    (currentPoint.Y == rect.Top || currentPoint.Y == rect.Bottom)
+                )
+                {
+                    MessageBox.Show("Точка находится на границе прямоугольника.");
+                }
+                else
+                {
+                    MessageBox.Show("Точка находится снаружи прямоугольника.");
+                }
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                string clientSizeInfo = $"Ширина = {this.ClientSize.Width}, Высота = {this.ClientSize.Height}";
+                this.Text = clientSizeInfo;// Установление нового заголовка
+
+                // Через некоторое время (с использованием таймера), установка оригинального заголовка обратно
+                System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+                timer.Interval = 10000; // Через 10 секунды вернем оригинальный заголовок
+                timer.Tick += (s, args) =>
+                {
+                    this.Text = originalTitle; // Восстановление оригинального заголовка
+                    timer.Stop();
+                };
+                timer.Start();
+            }
+        }
+
+        private void MMEvent(object sender, MouseEventArgs e)
+        {
+            // Получаем текущие координаты мыши
+            int mouseX = e.Location.X;
+            int mouseY = e.Location.Y;
+
+            // Форматируем текст для отображения в заголовке
+            string mouseCoordinates = $"X = {mouseX}, Y = {mouseY}";
+
+            // Устанавливаем отформатированный текст в заголовок окна
+            this.Text = mouseCoordinates;
+
+            lb_cord.Text = $"Mouse Move: { e.Location.X} : { e.Location.Y}";
+        }
+
+        private void KD_Form(object sender, KeyEventArgs e)
+        {
+            isCTRLdown = true;
+        }
+
+        private void KU_Form(object sender, KeyEventArgs e)
+        {
+            isCTRLdown = false;
         }
     }
 }
